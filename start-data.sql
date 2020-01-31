dffa58a5656798ace82580ff94d92652a2d02374
Dane Schmidt <daneschmidt@gmail.com>
2020-01-31 13:19:48 -0600

    initial commit docker setup

---
+++ start-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+VALUES 
+('Myron', 'Male', 10, FALSE, 'doesnt like when you ask too many questions.'),
+('Scott', 'Male', 11, FALSE, 'is fucking chilling.'),
+('Rach', 'Female', 8, FALSE, 'wants everyone to have a job.');
